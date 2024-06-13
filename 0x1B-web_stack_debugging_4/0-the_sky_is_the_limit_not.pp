f68e2eac8010569077c4ed45efafb5f3a06fef53
israadam <israaosama796@gmail.com>
2024-06-13 14:29:17 -0700

    p

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,5 @@
-ncreases the amount of traffic an Nginx server can handle.
-
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+# fix nginx to accept and serve more requests
+exec {'modify max open files limit setting':
+ command => 'sed -i "s/4096/15/" /etc/default/nginx && sudo service nginx restart',
+ path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
